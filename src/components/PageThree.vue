81152039d4c0dd36b459b30bc3c2b6f19a770adf
Connor Frendt <connor.frendt@pollinate.com>
2019-12-04 16:42:57 -0800

    working on generating empty boxes to put answers

--- src/components/PageThree.vue
+++ src/components/PageThree.vue
@@ -4,10 +4,11 @@
             v-model="wordBox"
             @end="onEnd"
         >
-            <transition-group type="transition">
+            <transition-group id="bar">
                 <div
                     v-for="item in wordBox"
                     :key="item"
+                    class="foo"
                 >
                     <div>{{ item }}</div>
                 </div>
@@ -22,7 +23,10 @@ import draggable from 'vuedraggable';
 export default {
     data() {
         return {
-            wordBox: ['Food', 'Water', 'Shelter'],
+            wordBox: [
+                'Housing', 'Food', 'Clothing', 'Water', 'Electricity/Gas', 'Transportation', 'Basic Phone Plan', 'Emergency Savings', 'Insurance',
+                'Games', 'Coffehouse Drinks', 'Eating Out', 'Smartphone', 'Brand Name/Designer Clothing', 'Theater Movies/Concerts', 'Netflix/Hulu/Amazon Prime', 'Spotify/Pandora/Google Play Music', 'Brand Name Products', 'Luxury Vehicles', 'Travel', 'Gym Memberships'
+            ],
             oldIndex: '',
             newIndex: ''
         }
@@ -32,7 +36,6 @@ export default {
     },
     methods: {
         onEnd(event) {
-            console.log(event);
             this.oldIndex = event.oldIndex;
             this.newIndex = event.newIndex;
         }
@@ -41,6 +44,11 @@ export default {
 </script>
 
 <style>
+    #bar {
+        display: grid;
+        grid-template: repeat(3, 1fr) / repeat(7, 1fr);
+    }
+
     .foo {
         border: 1px solid black;
     }
