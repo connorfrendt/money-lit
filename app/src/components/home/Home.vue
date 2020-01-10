0ac73539b94e831cd60e552640a1bf6b9a1421c1
Connor Frendt <connor.frendt@pollinate.com>
2020-01-09 16:29:16 -0800

    wip

--- app/src/components/home/Home.vue
+++ app/src/components/home/Home.vue
@@ -2,16 +2,18 @@
     <div id="home">
         <h2>MoneyLit:</h2>
         <p>Understanding the difference between <i>needs</i> and <i>wants</i> helps you be strategic about your spending.</p>
-        <button @click="$emit('login', pizzaLogIn)">TO LOG IN</button>
+        <button @click="changeBoolean() && $emit('login', pizzaLogIn)">TO LOG IN</button>
     </div>
 </template>
 
 <script>
 export default {
     props: {
-        pizzaLogIn: false
+        pizzaLogIn: {
+            type: Boolean
+        }
     },
-    computed: {
+    methods: {
         changeBoolean() {
             this.pizzaLogIn = true;
         }
