b65de5517ba0ddc099c9c9e4d27b1dfe07dad425
Connor Frendt <connor.frendt@pollinate.com>
2020-01-17 14:26:25 -0800

    potentially fixed router login issue

--- app/src/components/App.vue
+++ app/src/components/App.vue
@@ -7,27 +7,13 @@
             </span>
         </header>
         <main>
-            <!-- <Home
-                v-if="user"
-                :user="user"
-            /> -->
-            <RouterView
-                v-if="user"
-                :user="user"
-            />
-            <Auth
-                v-if="!user"
-                :onSignUp="handleSignUp"
-                :onSignIn="handleSignIn"
-            />
+            <Home />
         </main>
     </div>
 </template>
 
 <script>
-import api from '../services/api';
-// import Home from './home/Home';
-import Auth from './auth/Auth';
+import Home from './home/Home';
 
 export default {
     data() {
@@ -36,46 +22,7 @@ export default {
         };
     },
     components: {
-        // Home,
-        Auth
-    },
-    created() {
-        const json = window.localStorage.getItem('profile');
-        if(json) {
-            this.setUser(JSON.parse(json));
-        }
-    },
-    methods: {
-        handleSignUp(profile) {
-            return api.signUp(profile)
-                .then(user => {
-                    this.setUser(user);
-                });
-        },
-        handleSignIn(credentials) {
-            return api.signIn(credentials)
-                .then(user => {
-                    this.setUser(user);
-                });
-        },
-        setUser(user) {
-            this.user = user;
-            if(user) {
-                api.setToken(user.id);
-                window.localStorage.setItem('profile', JSON.stringify(user));
-            }
-            else {
-                api.setToken();
-                window.localStorage.removeItem('profile');
-            }
-        },
-        changeName(name) {
-            return api.normalize(name);
-        },
-        handleLogout() {
-            this.setUser(null);
-            this.$router.push('/');
-        }
+        Home
     }
 };
 </script>
--- app/src/components/home/Home.vue
+++ app/src/components/home/Home.vue
@@ -1,14 +1,73 @@
 <template>
     <div id="home">
-        <h2>MoneyLit:</h2>
-        <p>Understanding the difference between <i>needs</i> and <i>wants</i> helps you be strategic about your spending.</p>
-        <button><RouterLink to="/Auth">TO LOG IN</RouterLink></button>
+        <div v-if="$route.path === '/'">
+            <h2>MoneyLit:</h2>
+            <p>Understanding the difference between <i>needs</i> and <i>wants</i> helps you be strategic about your spending.</p>
+            <button><RouterLink to="/Auth">TO LOG IN</RouterLink></button>
+        </div>
+        <RouterView v-else />
+        <!-- <Auth
+            v-if="user"
+        />
+        <PageTwo
+            v-else
+        /> -->
     </div>
 </template>
 
 <script>
+// import Auth from '../auth/Auth';
+// import PageTwo from '../PageTwo';
+import api from '../../services/api';
+
 export default {
-    
+    data() {
+        return {
+            user: null
+        };
+    },
+    components: {
+        // Auth,
+        // PageTwo
+    },
+    created() {
+        const json = window.localStorage.getItem('profile');
+        if(json) {
+            this.setUser(JSON.parse(json));
+        }
+    },
+    methods: {
+        handleSignUp(profile) {
+            return api.signUp(profile)
+                .then(user => {
+                    this.setUser(user);
+                });
+        },
+        handleSignIn(credentials) {
+            return api.signIn(credentials)
+                .then(user => {
+                    this.setUser(user);
+                });
+        },
+        setUser(user) {
+            this.user = user;
+            if(user) {
+                api.setToken(user.id);
+                window.localStorage.setItem('profile', JSON.stringify(user));
+            }
+            else {
+                api.setToken();
+                window.localStorage.removeItem('profile');
+            }
+        },
+        changeName(name) {
+            return api.normalize(name);
+        },
+        handleLogout() {
+            this.setUser(null);
+            this.$router.push('/');
+        }
+    }
 };
 </script>
 
